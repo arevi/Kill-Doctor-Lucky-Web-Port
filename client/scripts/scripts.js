96d69a4464fd8faab8d4843a6b0214cf8297aa3c
Arevi <Arevi.Dev@gmail.com>
2019-09-05 10:12:26 -0400

    Kren CSS Grid Implementation of game board

---
+++ client/scripts/scripts.js
@@ -0,0 +1,20 @@
+class Player {
+  constructor() {
+    this.damage = 0;
+  }
+}
+
+class DoctorLucky {
+  constructor() {
+    this.health = 100;
+  }
+}
+
+const player1,
+  player2,
+  player3,
+  player4 = new Player();
+
+const doctorLucky = new DoctorLucky();
+
+
