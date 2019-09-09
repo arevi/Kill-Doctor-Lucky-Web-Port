c7aeef6f9dafacf6f05d39294027f3a2fa0412dc
Arevi <Arevi.Dev@gmail.com>
2019-09-09 13:17:29 -0400

    Adjusting CSS Grid to utilize background images isntead of img elements, allowing to turn css grid tiles into flex containers for later use. Bootstrapping initial object structure for storing game data, TBC

--- client/scripts/scripts.js
+++ client/scripts/scripts.js
@@ -1,20 +1,171 @@
-class Player {
-  constructor() {
-    this.damage = 0;
-  }
-}
-
-class DoctorLucky {
-  constructor() {
-    this.health = 100;
-  }
-}
-
-const player1,
-  player2,
-  player3,
-  player4 = new Player();
-
-const doctorLucky = new DoctorLucky();
-
-
+var assets = {
+  rooms: [
+    {
+      id: 0,
+      name: 'Drawing Room',
+      tileName: '',
+      lineOfSight: [1, 3, 5, 19, 20, 22],
+      visitableRooms: [1, 3, 5, 20]
+    },
+    {
+      id: 1,
+      name: 'Parlor',
+      tileName: '',
+      lineOfSight: [0, 2, 5, 16, 18],
+      visitableRooms: [0, 2, 14, 15, 16, 17, 18]
+    },
+    {
+      id: 2,
+      name: 'Billiard Room',
+      tileName: '',
+      lineOfSight: [1, 3, 4, 22],
+      visitableRooms: [1, 3, 14, 15, 16, 17, 18]
+    },
+    {
+      id: 3,
+      name: 'Dining Hall',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 4,
+      name: 'Sitting Room',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 5,
+      name: 'Trompy Room',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 6,
+      name: 'Green House',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 7,
+      name: 'Winter Garden',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 8,
+      name: 'Kitchen',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 9,
+      name: 'Lancaster Room',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 10,
+      name: 'Master Suite',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 11,
+      name: 'Nursery',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 12,
+      name: 'Armory',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 13,
+      name: 'Library',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 14,
+      name: 'Tennessee Room',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 15,
+      name: 'Lilac Room',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 16,
+      name: 'Servants Quarters',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 17,
+      name: 'Hedge Maze',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 18,
+      name: 'Carriage House',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 19,
+      name: 'Piazza',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 20,
+      name: 'Foyer',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 21,
+      name: 'Wine Cellar',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 22,
+      name: 'Gallery',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    },
+    {
+      id: 23,
+      name: 'White Room',
+      tileName: '',
+      lineOfSight: [],
+      visitableRooms: []
+    }
+  ]
+};
